22a97d2b48e18a08bc96c289219d5e3749e20a05
Cobeannn <colby10cheese@gmail.com>
2026-09-23 19:39:29 +1200

    Customer purchase

--- video-game/scripts/customer.gd
+++ video-game/scripts/customer.gd
@@ -2,17 +2,31 @@ extends Area2D
 
 var shelves
 var sprite
+var money_counter
+var want_unlocked = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	shelves = get_tree().get_nodes_in_group("shelf")
 	sprite = get_tree().get_first_node_in_group("sprite")
+	money_counter = get_tree().get_first_node_in_group("money_counter")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	for shelf in shelves: 
+		if get_overlapping_areas():
+			for overlapping_body in get_overlapping_areas():
+				if overlapping_body.is_in_group("shelf"):
+					if overlapping_body.is_stocked:
+						if not overlapping_body.is_in_group("closed"):
+							want_unlocked = false
+							money_counter.money += 40
+							overlapping_body.is_stocked = false
+						elif overlapping_body.is_in_group("closed"):
+							want_unlocked = true
 
 
 func squish(squish_amount,squish_time):
--- video-game/scripts/shelf.gd
+++ video-game/scripts/shelf.gd
@@ -6,6 +6,11 @@ var stock_box
 var in_stock
 var stock
 var touching_shelf
+var customer_touching
+var customer
+var timers
+var timer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,17 +19,38 @@ func _ready() -> void:
 	in_stock = {}
 	player = get_tree().get_first_node_in_group("player")
 	stock_box = get_tree().get_first_node_in_group("stock")
+	customer = get_tree().get_nodes_in_group("customer")
 	add_to_group("shelf")
+	timers =  get_tree().get_nodes_in_group("timer")
+	timer = timers[1]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if get_overlapping_areas():
+		var area = self
+		if area.is_in_group("customer"):
+			print("yup")
 	if get_overlapping_bodies():
 		touching_shelf = self
+		if is_in_group("closed"):
+			if Input.is_action_just_pressed("ui_interact"):
+				timer.start()
+				await timer.timeout
+				remove_from_group("closed")
+				await get_tree().create_timer(2).timeout
+				print(is_in_group("closed"))
+				add_to_group("closed")
+			elif Input.is_action_just_released("ui_interact"):
+				print("this is no longer a pressing matter")
+				timer.stop()
 	elif not get_overlapping_bodies():
 		touching_shelf = null
 	if Input.is_action_just_pressed("ui_check"):
 		print(is_stocked)
+	if not is_stocked:
+		if get_child(2):
+			get_child(2).queue_free()
 
 
 func place(place_items):
